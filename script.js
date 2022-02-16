d3c162dc3af37c447d9573cd2a0e127dbfc7a6d9
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-02-16 11:21:08 -0300

    atividade: criado layout de botao grande quando nao tem quiz e botao pequeno quando tem, criada funcao que faz redireciona para terceira tela quando um dos botoes de criar quiz e acionado

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ function obterQuizzes(){
 
 function renderizaQuizzes(resposta){
     const quizzes = resposta.data
-    console.log('quizzes: ', quizzes)
-    console.log('quizzes: ', quizzes[0].title)
     const lista = document.querySelector('.primeira-tela ul')
     quizzes.forEach(element => {
-        lista.innerHTML += `<li> <span class="titulos"  onclick="abrirQuizz()">${element.title}</span> <span class="efeitos" onclick="abrirQuizz()"></span> <img src="${element.image}"></li>`  
+        lista.innerHTML += `<li> <span class="titulos"  onclick="abrirQuiz()">${element.title}</span> <span class="efeitos" onclick="abrirQuiz()"></span> <img src="${element.image}"></li>`  
     })
 }
 
@@ -18,8 +16,23 @@ function erroAoObterQuizzes(){
     console.log("erro ao carregar quizzes")
 }
 
-function abrirQuizz(){
+function abrirQuiz(){
     document.querySelector('.primeira-tela').classList.add('esconder')
+    document.querySelector('.ir-para-criacao').classList.add('esconder')
+}
+
+//vai ser chamada quando o usuário já tiver quizzes
+function primeiraTelaComQuizCriado(){
+    document.querySelector('.ir-para-criacao').classList.add('esconder')
+    const primeiraTela = document.querySelector('.primeira-tela p')
+    primeiraTela.innerHTML = `<div class="seus-quizzes"><p>Seus Quizzes</p> <ion-icon name="add-circle"></ion-icon></div>`
+}
+
+function criarQuiz(){
+    document.querySelector('.primeira-tela').classList.add('esconder')
+    document.querySelector('.ir-para-criacao').classList.add('esconder')
+    document.querySelector('.terceira-tela').classList.remove('esconder')
+
 }
 
 obterQuizzes()
