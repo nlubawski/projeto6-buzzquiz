2b7604e2a36ce0ee197aa6e95a36bd7f83460000
LucasG <lucaslguarnieri@gmail.com>
2022-02-18 16:17:52 -0300

    feat: comportamento das repostas ao se responder o quiz

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function renderizaQuizzes(resposta){
     const quizzes = resposta.data
     const lista = document.querySelector('.primeira-tela ul')
     quizzes.forEach(element => {
-        lista.innerHTML += `<li> <span class="titulos"  onclick="abrirQuiz(${element.id})">${element.title}</span> <span class="efeitos" onclick="abrirQuiz(${element.id})"></span> <img src="${element.image}"></li>`  
+        lista.innerHTML += `<li> 
+        <span class="titulos"  onclick="abrirQuiz(${element.id})">
+        ${element.title}
+        </span> <span class="efeitos" onclick="abrirQuiz(${element.id})">
+        </span> <img src="${element.image}">
+        </li>`  
     })
 }
 
@@ -27,18 +32,71 @@ function abrirQuiz(id){
 
 function renderizaQuiz(resposta){
     const quiz = resposta.data
+    const perguntas = quiz.questions
     const segundaTela = document.querySelector('.segunda-tela')
-    segundaTela.innerHTML = `<div class="cabecalho-quiz">
+    segundaTela.innerHTML = `
+    <div class="cabecalho-quiz">
     <div class="gradiente"></div>
-    <div class="titulo-cabecalho">${quiz.title}</div>
-    <img src="${quiz.image}" class="imagem-cabecalho" alt="imagem do cabeçalho-quizz"></img>
-    <div>
-    <ul></ul>`
+    <span class="titulo-cabecalho">${quiz.title}</span>
+    <img src="${quiz.image}" class="imagem-cabecalho" alt="imagem do cabeçalho-quizz">
+    </div>
+    <ul class="perguntas">${renderizaPerguntas(perguntas)}</ul>`
+    
 }
-
 
 function erroAoObterQuiz(){
     console.log("erro ao carregar quizzes")
+}
+
+function renderizaPerguntas(perguntas){
+    let todasAsPerguntas = ""
+    perguntas.forEach(pergunta => {
+        todasAsPerguntas +=`
+        <section class='box-pergunta'>
+            <div class="titulo-pergunta" style="background-color:${pergunta.color};">${pergunta.title}</div>
+            <div class="box-respostas">
+                <div class="gradiente-branco esconder"></div>
+                ${renderizaRespostas(pergunta.answers.sort(comparador))}
+            </div>
+        </section>`
+    })
+    return todasAsPerguntas
+}
+
+function renderizaRespostas(respostas){
+    let todasAsRespostas = ""
+    respostas.forEach(resposta => {
+        if (resposta.isCorrectAnswer){
+            todasAsRespostas += `
+            <div class="box-resposta" onclick=" selecionaReposta(this); selecionaBoxResposta(this.parentNode); this.onclick=null">
+            <img src="${resposta.image}" class="imagem-resposta" alt="imagem de uma resposta">
+            <spam class="texto-resposta texto-verde texto-preto">${resposta.text}</span>
+            </div>`
+        }else{
+            todasAsRespostas += `
+            <div class="box-resposta" onclick=" selecionaReposta(this); selecionaBoxResposta(this.parentNode); this.onclick=null ">
+            <img src="${resposta.image}" class="imagem-resposta" alt="imagem de uma resposta">
+            <spam class="texto-resposta texto-vermelho texto-preto">${resposta.text}</span>
+            </div>`
+        }
+    })
+    return todasAsRespostas
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function  selecionaReposta(boxResposta){
+    boxResposta.style.zIndex = 3;
+}
+
+function selecionaBoxResposta(boxResposta){
+    const filhosBoxResposta = boxResposta.querySelectorAll("div.gradiente-branco, spam");
+    filhosBoxResposta.forEach(elemento => {
+        elemento.classList.remove("esconder")
+        elemento.classList.remove("texto-preto")
+    })
 }
 
 //vai ser chamada quando o usuário já tiver quizzes
