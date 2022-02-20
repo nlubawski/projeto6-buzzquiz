001b8197edaba24ca3932c7feed987d252053c05
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-02-20 16:47:40 -0300

    ajustes: ajustes na exibicao da tela inicial

--- script.js
+++ script.js
@@ -38,11 +38,26 @@ function renderizaQuizzes(resposta){
     })
 }
 
+function renderizaQuiz(resposta){
+    const quiz = resposta.data
+    const perguntas = quiz.questions
+    numeroDeQuestoes = perguntas.length 
+    levelsDeAcerto = quiz.levels
+    const segundaTela = document.querySelector('.segunda-tela')
+    segundaTela.innerHTML = `
+    <div class="cabecalho-quiz">
+    <div class="gradiente"></div>
+    <span class="titulo-cabecalho">${quiz.title}</span>
+    <img src="${quiz.image}" class="imagem-cabecalho" alt="imagem do cabeçalho-quizz">
+    </div>
+    <ul class="perguntas">${renderizaPerguntas(perguntas)}</ul>`
+    
+}
+
 function erroAoObterQuizzes(){
     console.log("erro ao carregar quizzes")
 }
 
-//modifiquei a função abrirQuiz para receber com parametro o id do quiz que ela abrirá. A função renderizaQuizzes passa a passar como parametro o element.id para a função onClick das <li>  //apagar na versão final//
 function abrirQuiz(id){
     document.querySelector('.primeira-tela').classList.add('esconder')
     document.querySelector('.ir-para-criacao').classList.add('esconder')
@@ -187,9 +202,9 @@ function finalDoQuiz(){
     <button class="voltar-home" onclick="limpaSegundaTela();  document.location.reload()">Voltar para home</button>
     `
     document.querySelector(".imagem-level").scrollIntoView()
- }
+}
 
- function limpaSegundaTela(){
+function limpaSegundaTela(){
     levelsDeAcerto = []
     numeroDeQuestoes = 0
     questoesRespondidas = 0
@@ -197,13 +212,13 @@ function finalDoQuiz(){
     const segundaTela = document.querySelector('.segunda-tela')
     segundaTela.innerHTML = ""
     
- }
+}
 
-//vai ser chamada quando o usuário já tiver quizzes
+
 function primeiraTelaComQuizCriado(){
     document.querySelector('.ir-para-criacao').classList.add('esconder')
-    const primeiraTela = document.querySelector('.primeira-tela p')
-    primeiraTela.innerHTML = `<div class="seus-quizzes"><p>Seus Quizzes</p> <ion-icon name="add-circle"></ion-icon></div>`
+    const primeiraTela = document.querySelector('.meus-quizzes')
+    //primeiraTela.innerHTML = `<div class="meus-quizzes-topo"><p>Todos os Quizes</p> <ion-icon name="add-circle" onclick="criarQuiz()"></ion-icon></div>`
 }
 
 function criarQuiz(){
@@ -464,7 +479,7 @@ function finalizarQuiz(resposta){
 function voltarPraHome(){
     document.querySelector('.terceira-tela__quarta').classList.add('esconder')
     document.querySelector('.primeira-tela').classList.remove('esconder')
-    document.querySelector('.ir-para-criacao').classList.remove('esconder')
+    primeiraTelaComQuizCriado()
     window.location.reload()
 }
 
